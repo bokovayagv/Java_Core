9f24de92909f3257e4434f3afc58a95625b68f45
bokovayagv <bokovayagv@gmail.com>
2016-06-05 12:41:58 +0300

    Module_1

---
+++ Module_1/home/dz1/src/bokovayagv/m1/dz1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by aleigis on 29.05.2016.
+ */
+public class dz1 {
+    public static void main(String[]args)
+    {
+        System.out.println("Hi, I'm first Java program!Stay tuned!");
+
+    }
+
+}
